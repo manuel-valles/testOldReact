c2dbef730bd291c1761f968e9515e02ff819a62d
manukempo <manukempo@gmail.com>
2017-08-21 12:56:02 +0100

    Setup render react

---
+++ client/index.js
@@ -0,0 +1,6 @@
+// Import React
+import React from 'react';
+import {render} from 'react-dom';
+import App from './components/App';
+
+render(<App />, document.getElementById('app'));
--- server/index.js
+++ server/index.js
@@ -2,9 +2,16 @@
 import express from 'express';
 // Path for the HTML file
 import path from 'path';
+// Import webpack; its Middleware for bundling; and the config of the compiler
+import webpack from 'webpack';
+import webpackMiddleware from 'webpack-dev-middleware';
+import webpackConfig from '../webpack.config.dev';
 
 // Initialize out app as express
 let app = express();
+
+// Use the Middleware package that takes a compiler
+app.use(webpackMiddleware(webpack(webpackConfig)));
 
 // Get all routes with an error function
 app.get('/*', (req, res)=>{
